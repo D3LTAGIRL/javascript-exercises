c008780cb060ce0ebda4849382728da944ee35a5
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-09-11 00:40:01 -0500

    Finish 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returnString = ''
+    for (let i = 0; i < string.length; i++) {
+        returnString += string[string.length-i-1]
+    }
+    return returnString
 };
 
 // Do not edit below this line
