604cc33cdf7a9062deb5580daf355c8191bcd0c1
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-09-25 20:04:58 -0500

    Finish 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  for (let i = 1; i < arguments.length; i++) {
+    while (array.indexOf(arguments[i]) != -1) {
+      array.splice(array.indexOf(arguments[i]), 1)
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
