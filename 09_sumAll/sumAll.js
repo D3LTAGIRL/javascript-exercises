e442040bee96f8f98374062ba8efa623228305bd
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-09-25 20:05:09 -0500

    Finish 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  // check if parameters are non integer
+  if (!Number.isSafeInteger(int1) || !Number.isSafeInteger(int2))
+    return "ERROR";
+  // check if parameters are negative
+  if (int1 < 0 || int2 < 0)
+    return "ERROR";
+  const args = [...arguments];
+  args.sort();
+  sum = 0;
+  for (let i = args[0]; i <= args[1]; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
