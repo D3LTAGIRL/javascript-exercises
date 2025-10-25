e9f9dd619255e3aa9123ab180a06150c8140a67b
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-10-25 18:01:43 -0500

    Finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((currOldest, person) => {
+    person.yearOfDeath ??= new Date(Date.now()).getFullYear();
+    currOldest.yearOfDeath ??= new Date(Date.now()).getFullYear();
 
+    let currOldestAge = currOldest.yearOfDeath - currOldest.yearOfBirth;
+    let personAge = person.yearOfDeath - person.yearOfBirth;
+
+    return currOldestAge < personAge ? person : currOldest;
+  })
 };
 
 // Do not edit below this line
